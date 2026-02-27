ed95845fb3e4d8e336d1a333156e65ab96d979b9
Gustavohp1708 <Gustavohp1708@users.noreply.github.com>
2026-02-27 16:39:21 -0300

    Inserir cadeias com variável chain

--- agente.py
+++ agente.py
@@ -2,12 +2,23 @@ import os
 from dotenv import load_dotenv
 from rich import print
 from langchain_groq import ChatGroq
+from langchain_core.prompts import ChatPromptTemplate
 
 load_dotenv()
 
-llm = ChatGroq(model=os.getenv("GROQ_MODEL"))
+llm = ChatGroq(model=os.getenv("GROQ_MODEL"), temperature=0.4)
+prompt = ChatPromptTemplate.from_template(
+    "Você é um assistente de IA que responde a perguntas de forma clara e concisa. " 
+    "Pergunta: {question}"
+)
 
-print(llm.invoke("Estou testando LangSmith neste momento"))
+def chamar_llm(texto):
+    chain = prompt | llm
+    resultado = chain.invoke({"question": texto})
+    return resultado.content
+
+if __name__ == "__main__":
+    print(chamar_llm("Olá LangSmith!"))
 
 
 
